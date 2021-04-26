56686c4d7e2c9dfb4bec5e43521ddb4f5d4259da
shahram <shahram@newrelic.com>
2021-04-26 15:31:11 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# nri-bosh-release-xenial
+A bosh release for deploying the New Relic Infrastructure agent 
+
+This is a self contained bosh release for installing the New Relic Infrastructure agent. It includes the deb package as a blob and has an install script that installs the package. The package is the install package for Ubuntu Xenial.
+
+The release is meant to be installed as an addon. In order to facilitate that, a sample runtime.yml file is included.
+
+## Upload the release
+
+To use, upload the release:
+```bash
+bosh upload-release ~/Downloads/nr-bosh-release-xenial-1.2.1.c.tgz
+```
+
+## Update license key
+
+In runtime.yml, modify the `license_key` parameter to be the license key for the target New Relic account.
+
+## Update runtime config
+
+...and then, update the runtime config:
+```bash
+bosh update-runtime-config runtime.yml
+```
+
+Finally, redeploy to pick up the addon.
---
+++ jobs/install-nri/templates/nri.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+export NRIA_LICENSE_KEY=<%= p('infra_agent.license_key') %>
+
+echo "$(date '+%F %T') Pre-start Job for New Relic Infrastructure"
+
+if [ -e /usr/bin/newrelic-infra ]
+then
+  echo "$(date '+%F %T') Infrastructure is already installed. Version: $(/usr/bin/newrelic-infra -version)"
+fi
+
+echo "$(date '+%F %T') Installing New Relic Infrastructure"
+
+echo -e "license_key: ${NRIA_LICENSE_KEY}\n" | tee /etc/newrelic-infra.yml
+
+echo -e "display_name: <%= spec.name + '-' + spec.id %>\n" |  tee -a /etc/newrelic-infra.yml
+
+echo -e "log_file: /var/vcap/sys/log/install-nri/newrelic-infa.log\n" | tee -a /etc/newrelic-infra.yml
+
+
+<% if_p('infra_agent.agent_props') do |settings| -%>
+  <% settings.each do |key, value| -%>
+echo -e "<%= key %>: <%= value %>" | tee -a /etc/newrelic-infra.yml
+  <% end %>
+echo -e | tee -a /etc/newrelic-infra.yml
+<% end %>
+
+echo -e "custom_attributes:" | tee -a /etc/newrelic-infra.yml
+echo -e "  bosh.az: <%= spec.az %>" | tee -a /etc/newrelic-infra.yml
+echo -e "  bosh.bootstrap: <%= spec.bootstrap %>" | tee -a /etc/newrelic-infra.yml
+echo -e "  bosh.deployment: <%= spec.deployment %>" | tee -a /etc/newrelic-infra.yml
+echo -e "  bosh.id: <%= spec.id %>" | tee -a /etc/newrelic-infra.yml
+echo -e "  bosh.index: <%= spec.index %>" | tee -a /etc/newrelic-infra.yml
+echo -e "  bosh.ip: <%= spec.ip %>" | tee -a /etc/newrelic-infra.yml
+echo -e "  bosh.name: <%= spec.name %>" | tee -a /etc/newrelic-infra.yml
+echo -e "  bosh.environment: <%= p('infra_agent.environment') %>" | tee -a /etc/newrelic-infra.yml
+
+<% if_p('infra_agent.custom_attributes') do |cas| %>
+  <% cas.each do |key, value| -%>
+     <% unless value.empty? -%>
+echo -e "  bosh.<%= key %>: <%= value %>" | tee -a /etc/newrelic-infra.yml
+     <% end %>
+  <% end %>
+<% end %>
+
+wait_for_dpkg_lock() {
+  while :
+  do
+      if ! [[ `lsof /var/lib/dpkg/lock` ]]
+      then
+          break
+      fi
+      echo "sleeping 3 in wait_for_dpkg_lock"
+      sleep 3
+  done
+}
+
+while :
+do
+  (
+      flock -x 200
+      sudo dpkg --force-confdef -i /var/vcap/packages/nr-infra/newrelic-infra_1.5.66_amd64.deb
+  ) 200>/var/vcap/data/dpkg.lock
+  if [ $? -ne 0 ] ; then
+    echo "Install failed. Calling wait_for_dpkg_lock"
+    wait_for_dpkg_lock
+  else
+     break
+  fi
+
+done
